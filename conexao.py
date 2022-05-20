db6ecb10d0ab7dd1415516f87ca4ec6d0fbb1f0b
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-05-20 13:59:06 -0300

    criando a tabela do bd

--- conexao.py
+++ conexao.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+
 def ConexaoBanco():
     caminho = r'C:\cursopython\CFB_Cursos\Banco\agenda_proj.db'
     con = None
@@ -9,3 +10,24 @@ def ConexaoBanco():
     except Error as e:
         print(e)
     return con
+
+
+def criar_tabela(conexao, sql):
+    try:
+        c = conexao.cursor()
+        c.execute(sql)
+        print('Tabela criada')
+    except Error as e:
+        print(e)
+
+
+vsql = """
+    CREATE TABLE tb_contatos(
+        N_IDCONTATO INTEGER PRIMARY KEY AUTOINCREMENT,
+        T_NOMECONTATO VARCHAR(30),
+        T_TELEFONECONTATO VARCHAR(14),
+        T_EMAILCONTATO VARCHAR(30)        
+    );
+"""
+vcon = ConexaoBanco()
+criar_tabela(vcon, vsql)
